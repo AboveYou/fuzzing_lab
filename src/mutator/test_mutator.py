e3b871eca27dc3bd837074bd881a3664961e8872
Sebastian Schirle <60340491+AboveYou@users.noreply.github.com>
2023-06-18 19:07:40 +0200

    add all version numbers in text_mutator

--- src/mutator/test_mutator.py
+++ src/mutator/test_mutator.py
@@ -1,5 +1,7 @@
 import mutator_v1 as mut1
 import mutator_v2 as mut2
+import mutator_v3 as mut3
+import mutator_v4 as mut4
 import sys
 
 def cycle_fuzz(module):
@@ -17,6 +19,10 @@ def main():
             cycle_fuzz(mut1)
         case "2":
             cycle_fuzz(mut2)
+        case "3":
+            cycle_fuzz(mut3)
+        case "4":
+            cycle_fuzz(mut4)
         case _:
             print("invalid version number!")
 
